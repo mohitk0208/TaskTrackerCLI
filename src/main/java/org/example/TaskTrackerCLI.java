1b41e1682d2a4701a613f82e3e7eb5c2a568ddce
Mohit kumar <mohitk02082000@gmail.com>
2025-06-22 23:12:48 +0530

    Initial Commit

---
+++ src/main/java/org/example/TaskTrackerCLI.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class TaskTrackerCLI {
+    public static void main(String[] args) {
+        System.out.println("This is a simple Task Tracker CLI application.");
+    }
+}
